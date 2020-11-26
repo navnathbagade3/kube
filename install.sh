f3e951b4f163ffe86b21097c2c4a1cde58498833
navnathbagade3 <navnathbagade3@gmail.com>
2020-11-26 19:55:12 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,35 @@
 #!bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install azure-cli
-sudo apt-get install fzf
-sudo apt-get install tree
-sudo apt-get install unzip
+#installing packages
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install fzf -y
+sudo apt-get install tree -y
+sudo apt-get install unzip -y
+sudo apt-get install azure-cli -y
+sudo az aks install-cli -y
+
 sudo touch /usr/local/bin/kubectl /usr/local/bin/kubectx /usr/local/bin/kubens /usr/local/bin/helm3 /usr/local/bin/helm2
 sudo chmod 777 /usr/local/bin/*
-sudo az aks install-cli
 mkdir helm2 helm3
+
+#installing helm3 
+
 curl https://get.helm.sh/helm-v3.3.4-linux-amd64.tar.gz -o helm3.tar.gz
 tar xzfv helm3.tar.gz -C helm3
 sudo mv helm3/linux-amd64/helm /usr/local/bin/helm3
+
+#installing helm2
+
 curl https://get.helm.sh/helm-v2.16.12-linux-amd64.tar.gz -o helm2.tar.gz
 tar xzfv helm2.tar.gz -C helm2
 sudo mv helm2/linux-amd64/helm /usr/local/bin/helm2
+
+rm -rf helm2 helm3
+
+#installing terraform
+
 git clone https://github.com/tfutils/tfenv.git ~/.tfenv
 echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
 mkdir -p ~/.local/bin/
@@ -25,13 +39,21 @@ curl https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_am
 unzip terraform_0.12.24_linux_amd64.zip
 tfenv install 0.12.24
 tfenv use 0.12.24
-sudo apt-get update
-sudo apt-get upgrade
+
+#updating packages
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+#creating local symbolic links
 
 ln -s /mnt/c/Users/nbagade/Downloads/kube/install.sh ~/.install.sh
 ln -s /mnt/c/Users/nbagade/Downloads/notes.sh ~/.notes.sh
 ln -s /mnt/c/Users/nbagade/Downloads .downloads
 ln -s /mnt/c/Users/nbagade/Downloads/kube .repo
+
+
+#appending .bash_profile
 
 cat <<- 'EOF' >> "~/.bash_profile"
 if [ -n "$BASH_VERSION" ] && [ -f $HOME/.bashrc ];then
